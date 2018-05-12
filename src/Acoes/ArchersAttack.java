f8e8b38b0e83a62ad7dd650902cf00559c36b97b
Unknown <killlo325@hotmail.com>
2018-05-13 00:33:14 +0100

    V 0.41

--- src/Acoes/ArchersAttack.java
+++ src/Acoes/ArchersAttack.java
@@ -19,34 +19,41 @@ public class ArchersAttack {
            Dado dado = new Dado();
            int valor = 0;
            boolean acaoRealizada = false;
+           
             System.out.println("Archers Attack!\n");
             System.out.println("[1] - Ladders\n");
             System.out.println("[2] - Battering Ram\n");
             System.out.println("[3] - Siege Tower\n");
            
            
-            while(0==0){
+            while(!acaoRealizada){
                Scanner scan = new Scanner(System.in);
                int choice = scan.nextInt();
                valor = dado.rodaDado();
                     
-               if(choice == 1){
-                    if(valor + drm.getLadders() > ET.getStrenghtLadder() && ET.getPosLadder() != 4){
+               if(choice == 1 && ET.getPosLadder() != 4 ){
+                    if(valor + drm.getLadders() > ET.getStrenghtLadder()){
                         ET.setPosLadder(ET.getPosLadder() + 1);
                         System.out.println("Ataque às Ladders Efetivo");
                         acaoRealizada = true;
-                    }
-               }else if(choice == 2){
-                    if(valor + drm.getBatteringRam() > ET.getStrenghtBatteringRam() && ET.getPosBatteringRam() != 4){
+                    }else{
+                        System.out.println("Ataque ineficaz");
+                    } 
+               }else if(choice == 2 && ET.getPosBatteringRam() != 4){
+                    if(valor + drm.getBatteringRam() > ET.getStrenghtBatteringRam() ){
                         ET.setPosBatteringRam(ET.getPosBatteringRam() + 1);
                         System.out.println("Ataque às Battering Ram Efetivo");
                         acaoRealizada = true;
-                    }
-               }else if(choice == 3){
-                    if(valor + drm.getSiegeTower() > ET.getStrenghtSiegeTower() && ET.getPosSiegeTower() != 4){
+                    }else{
+                        System.out.println("Ataque ineficaz");
+                    } 
+               }else if(choice == 3  && ET.getPosSiegeTower() != 4){
+                    if(valor + drm.getSiegeTower() > ET.getStrenghtSiegeTower()){
                         ET.setPosSiegeTower(ET.getPosSiegeTower() + 1);
                         System.out.println("Ataque às Siege Tower");
                         acaoRealizada = true;
+                    }else{
+                        System.out.println("Ataque ineficaz");
                     } 
                 }
             }
