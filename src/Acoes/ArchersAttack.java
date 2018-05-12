0c3b22f2aa0ea336426585eb0ca992a919235df3
Unknown <killlo325@hotmail.com>
2018-05-13 00:24:38 +0100

    V 0.40c

--- src/Acoes/ArchersAttack.java
+++ src/Acoes/ArchersAttack.java
@@ -18,7 +18,7 @@ public class ArchersAttack {
     public void acao(EnemyTrack ET, DRM drm){
            Dado dado = new Dado();
            int valor = 0;
-
+           boolean acaoRealizada = false;
             System.out.println("Archers Attack!\n");
             System.out.println("[1] - Ladders\n");
             System.out.println("[2] - Battering Ram\n");
@@ -34,20 +34,20 @@ public class ArchersAttack {
                     if(valor + drm.getLadders() > ET.getStrenghtLadder() && ET.getPosLadder() != 4){
                         ET.setPosLadder(ET.getPosLadder() + 1);
                         System.out.println("Ataque às Ladders Efetivo");
-                        break;
+                        acaoRealizada = true;
                     }
                }else if(choice == 2){
                     if(valor + drm.getBatteringRam() > ET.getStrenghtBatteringRam() && ET.getPosBatteringRam() != 4){
                         ET.setPosBatteringRam(ET.getPosBatteringRam() + 1);
                         System.out.println("Ataque às Battering Ram Efetivo");
-                        break;
+                        acaoRealizada = true;
                     }
                }else if(choice == 3){
-                    if(valor + drm.getSiegeTower() > ET.getStrenghtSiegeTower() && ET.getStrenghtSiegeTower() != 4){
+                    if(valor + drm.getSiegeTower() > ET.getStrenghtSiegeTower() && ET.getPosSiegeTower() != 4){
                         ET.setPosSiegeTower(ET.getPosSiegeTower() + 1);
                         System.out.println("Ataque às Siege Tower");
-                        break;
-                } 
+                        acaoRealizada = true;
+                    } 
                 }
             }
     } 
